450d418b16b3a9cbfae26580fe60bb01443a405b
josephorokpo <josephorokpo@gmail.com>
2024-03-03 08:54:03 -0800

    Add 3-hbnb.js

---
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -0,0 +1,35 @@
+$(document).ready(function () {
+  let amenities = {};
+
+  $('input[type="checkbox"]').change(function () {
+    let amenityID = $(this).data('id');
+    let amenityName = $(this).data('name');
+
+    if ($(this).is(':checked')) {
+      amenities[amenityID] = amenityName;
+    } else {
+      delete amenities[amenityID];
+    }
+
+    $('.amenities h4').text(Object.values(amenities).join(', '));
+  });
+
+  $.post('http://0.0.0.0:5001/api/v1/places_search/', JSON.stringify({}), function(data) {
+    $.each(data, function(index, place) {
+      $('section.places').append('<article>\
+        <div class="title_box">\
+          <h2>' + place.name + '</h2>\
+          <div class="price_by_night">$' + place.price_by_night + '</div>\
+        </div>\
+        <div class="information">\
+          <div class="max_guest">' + place.max_guest + ' Guest' + (place.max_guest != 1 ? 's' : '') + '</div>\
+          <div class="number_rooms">' + place.number_rooms + ' Bedroom' + (place.number_rooms != 1 ? 's' : '') + '</div>\
+          <div class="number_bathrooms">' + place.number_bathrooms + ' Bathroom' + (place.number_bathrooms != 1 ? 's' : '') + '</div>\
+        </div>\
+        <div class="description">\
+          ' + place.description + '\
+        </div>\
+      </article>');
+    });
+  }, 'json');
+});
